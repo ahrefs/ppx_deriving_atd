a3f99d6e0d95b2627f8a780132e3d12282d095f6
Sewen Thy <me@sewenthy.dev>
2024-02-28 12:18:18 +0100

    use "json_string_of" rather than "string_of" since top-level

--- ppx_deriving_atd/test/test.ml
+++ ppx_deriving_atd/test/test.ml
@@ -1,19 +1,17 @@
 type x = { abc : string option; def : int; ghi : bool } [@@deriving atd_j]
 type y = { x : x } [@@deriving atd_j]
 
-[@@@end]
-
 open Test2
 
 type zz = { z : z; y : y } [@@deriving atd_j]
 
 let () =
   let x = { abc = None; def = 1; ghi = true } in
-  let x_str = string_of_x x in
+  let x_str = json_string_of_x x in
   print_endline x_str;
-  let x = x_of_string x_str in
-  print_endline @@ string_of_x x;
+  let x = x_of_json_string x_str in
+  print_endline @@ json_string_of_x x;
   (* print_endline @@ string_of_y { x }; *)
   let z = { zz = ""; zzz = 1; zzzz = false } in
-  print_endline @@ string_of_zz { z; y = { x } };
-  print_endline @@ Test2.string_of_z z
+  print_endline @@ json_string_of_zz { z; y = { x } };
+  print_endline @@ Test2.json_string_of_z z
